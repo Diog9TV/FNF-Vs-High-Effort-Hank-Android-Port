5a5ff3b23e69bbb7c470735d55239092435645cb
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-09-18 20:04:48 -0300

    Add files via upload

---
+++ assets/preload/data/mercenary/script.lua
@@ -0,0 +1,10 @@
+function onMoveCamera(focus)
+    if focus == 'dad' then
+        setProperty('camFollow.x', 400);
+	    setProperty('camFollow.y', 501.62);
+    else if focus == 'boyfriend' then
+        setProperty('camFollow.x', 1100);
+        setProperty('camFollow.y', 510.69);       
+    end        
+    end
+end
